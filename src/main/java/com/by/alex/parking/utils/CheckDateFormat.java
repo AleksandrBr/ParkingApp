0c37bcdbc1e87285491f6a0ffa8d9f2aafe6c1b7
Borkun, Oleksandr <Oleksandr.Borkun@Kronos.com>
2017-06-05 00:45:41 +0300

    add CheckDateFormat class + test

---
+++ src/main/java/com/by/alex/parking/utils/CheckDateFormat.java
@@ -0,0 +1,16 @@
+package com.by.alex.parking.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheckDateFormat {
+	private static Pattern pattern;
+	private static Matcher matcher;
+	private final static String TIME_FORMAT_PATTERN = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+	
+	public static boolean validate(String time) {
+	    pattern = Pattern.compile(TIME_FORMAT_PATTERN);
+	    matcher = pattern.matcher(time);
+	    return matcher.matches();
+	}
+}
---
+++ src/test/java/com/by/alex/parking/utils/CheckDateFormatTest.java
@@ -0,0 +1,21 @@
+package com.by.alex.parking.utils;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckDateFormatTest {
+	private final static String validTestData = "12:05";
+	private final static String invalidTestData = "23:66";
+
+	@Test(description = "checkDateFormatValidationPositive")
+	public void checkDateFormatValidationPositive(){
+		Assert.assertTrue(CheckDateFormat.validate(validTestData), "Check that time '" + validTestData + "' is correct");
+		
+	}
+
+	@Test(description = "checkDateFormatValidationNegative")
+	public void checkDateFormatValidationNegative(){
+		Assert.assertFalse(CheckDateFormat.validate(invalidTestData), "Check that time '" + invalidTestData + "' is incorrect");
+		
+	}
+}
